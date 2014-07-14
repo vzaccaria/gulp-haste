9720c6d233791e072bf07ef06c1f2e61a63cc5a5
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-07-14 20:48:22 +0200

    Fixed problem with compiler output

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 (function(){
-  var map, debug, sh, decodePath, encodePath, haste;
+  var map, debug, sh, path, os, moment, gutil, otm, cwd, getTmpFile, rmTmpFile, decodePath, encodePath, haste;
   map = require('map-stream');
   debug = require("debug")("gulp-haste");
   sh = require('shelljs');
+  path = require('path');
+  os = require('os');
+  moment = require('moment');
+  gutil = require('gulp-util');
+  otm = os.tmpdir != null ? os.tmpdir() : "/var/tmp";
+  cwd = process.cwd();
+  getTmpFile = function(){
+    var name, dire;
+    name = ("gulp-haste_" + moment().format('HHmmss') + "_") + Math.floor(Math.random() * 20000);
+    dire = otm + "/" + name;
+    return dire;
+  };
+  rmTmpFile = function(dir){
+    return shelljs.rm('-rf', dir);
+  };
   decodePath = function(file){
     var workdir, sourcefile, rpath, extname;
     workdir = file.cwd;
@@ -16,32 +31,39 @@
     };
   };
   encodePath = function(dirname, basename, extname){
-    return {
-      relative: path.join(dirname, basename + extname)
-    };
+    return path.join(dirname, basename + extname);
   };
   haste = function(options){
     return map(function(file, cb){
-      var ref$, dirname, basename, extname, newPath, imprt, cmd;
-      ref$ = decodePath(file), dirname = ref$.dirname, basename = ref$.basename, extname = ref$.extname;
-      extname = "js";
+      var cp, ref$, dirname, basename, extname, newPath, imprt, fil, cmd;
+      cp = (ref$ = decodePath(file), dirname = ref$.dirname, basename = ref$.basename, extname = ref$.extname, ref$);
+      extname = ".js";
       newPath = encodePath(dirname, basename, extname);
-      if (options['export'] == null) {
+      debug(JSON.stringify(cp, 0, 4));
+      if ((options != null ? options['export'] : void 8) == null) {
         imprt = "%%";
       } else {
         imprt = "Haste[" + options['export'] + "]";
       }
-      cmd = 'hastec --start="exports = ' + imprt + ';" ' + sourcefile + '--out=/dev/stdout';
+      fil = getTmpFile();
+      cmd = 'hastec --start="module.exports = ' + imprt + ';" ' + file.path + ' --out=' + fil;
       debug("invoking hastec compiler");
-      return sh.exec(cmd, function(code, output){
+      debug(cmd);
+      return sh.exec(cmd, {
+        silent: true
+      }, function(code){
+        var output;
         if (code !== 0) {
           return cb(true);
         } else {
-          file.path = path.join(file.base, newPath);
+          debug("Reading " + fil);
+          output = sh.cat(fil);
+          file.path = gutil.replaceExtension(file.path, '.js');
           file.contents = new Buffer(output);
           return cb(false, file);
         }
       });
     });
   };
+  module.exports = haste;
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -1,6 +1,22 @@
-map   = require('map-stream');
-debug = require("debug")("gulp-haste")
-sh = require('shelljs')
+map    = require('map-stream');
+debug  = require("debug")("gulp-haste")
+sh     = require('shelljs')
+path   = require('path')
+os     = require('os')
+moment = require('moment')
+gutil  = require('gulp-util')
+
+otm = if (os.tmpdir?) then os.tmpdir() else "/var/tmp"
+cwd = process.cwd()
+
+get-tmp-file = ->
+    name = "gulp-haste_#{moment().format('HHmmss')}_"+Math.floor(Math.random()*20000)
+    dire = "#{otm}/#{name}" 
+    return dire
+
+rm-tmp-file = (dir) ->
+    shelljs.rm '-rf', dir 
+
 
 decode-path = (file) ->
 
@@ -16,29 +32,36 @@ decode-path = (file) ->
   }
 
 encode-path = (dirname, basename, extname) ->
-  relative: path.join(dirname, basename+extname)
+  path.join(dirname, basename+extname)
 
 haste = (options) -> 
 
     map (file, cb) ->
 
-        { dirname, basename, extname } = decode-path(file)
-        extname                        = "js"
-        new-path                       = encode-path(dirname, basename, extname)
+        cp = { dirname, basename, extname } = decode-path(file)
+        extname = ".js"
+        new-path = encode-path(dirname, basename, extname)
+        debug(JSON.stringify(cp, 0 ,4))
 
-        if not options.export?
+        if not options?.export?
             imprt = "%%"
         else 
             imprt = "Haste[#{options.export}]"
 
-        cmd = 'hastec --start="exports = ' + imprt + ';" ' + sourcefile + '--out=/dev/stdout'
+        fil = get-tmp-file()
+        cmd = 'hastec --start="module.exports = ' + imprt + ';" ' + file.path + ' --out='+fil 
 
         debug("invoking hastec compiler")
-        sh.exec cmd, (code, output) ->
+        debug cmd
+        sh.exec cmd, {+silent}, (code) ->
             if code != 0
                 cb(true)
             else 
-                file.path     = path.join(file.base, new-path)
+                debug("Reading #fil")
+                output        = sh.cat(fil)
+                rm-tmp-file fil
+                file.path     = gutil.replaceExtension(file.path, '.js');
                 file.contents = new Buffer(output)
                 cb(false, file)
 
+module.exports = haste
